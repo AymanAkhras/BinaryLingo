d6ca9ea8a92cf5567c931679134fbfaec7d5f0ae
Angela Wang <wangela472@gmail.com>
2021-08-21 19:52:59 -0400

    Initial setup for Setup lesson and quiz

--- lib/screens/HomeScreen/HomeScreen.dart
+++ lib/screens/HomeScreen/HomeScreen.dart
@@ -1,46 +1,87 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:quiz_app/constants.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
-import 'package:quiz_app/screens/quiz/quiz_screen.dart';
+import 'package:quiz_app/screens/lessons/setuplesson.dart';
+import 'package:quiz_app/screens/quizzes/setupquiz.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Learn',
-          style: Theme.of(context)
-              .textTheme
-              .headline5
-              .copyWith(color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        centerTitle: true,
-        backgroundColor: Color(0xFF74D8DE),
-      ),
       body: Stack(
         children: [
           Container(
             decoration: new BoxDecoration(
               image: new DecorationImage(
-                image: new AssetImage("assets/Background-White.jpg"),
+                image: new AssetImage("assets/Background-Theme.jpg"),
                 fit: BoxFit.fill,
               ),
             ),
           ),
-          CustomScrollView(
-            slivers: <Widget>[
-              const SliverAppBar(
-                pinned: true,
-                expandedHeight: 250.0,
-                flexibleSpace: FlexibleSpaceBar(
-                  title: Text('Demo'),
-                ),
+          SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: kDefaultPadding),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Spacer(flex: 1), //2/6
+                  Center(
+                    child: Text(
+                      "Home",
+                      style: Theme.of(context).textTheme.headline3.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Spacer(flex: 1),
+                  Center(
+                    child: Text(
+                      "Computing Made Easy for Everyone",
+                      style: Theme.of(context).textTheme.headline6.copyWith(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Spacer(flex: 1),
+                  InkWell(
+                    onTap: () => Get.to(QuizScreen()),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding), // 15
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Login",
+                        style: Theme.of(context).textTheme.headline6.copyWith(
+                            color: Color(0xFF74D8DE),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 50),
+                  InkWell(
+                    onTap: () => Get.to(SetupLesson()),
+                    child: Container(
+                      width: double.infinity,
+                      alignment: Alignment.center,
+                      padding: EdgeInsets.all(kDefaultPadding), // 15
+                      decoration: BoxDecoration(
+                        gradient: kPrimaryGradient,
+                        borderRadius: BorderRadius.all(Radius.circular(12)),
+                      ),
+                      child: Text(
+                        "Register",
+                        style: Theme.of(context).textTheme.headline6.copyWith(
+                            color: Color(0xFF74D8DE),
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ),
+                  Spacer(flex: 2), // it will take 2/6 spaces
+                ],
               ),
-            ],
+            ),
           ),
         ],
       ),
